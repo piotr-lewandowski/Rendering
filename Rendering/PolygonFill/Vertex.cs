d6f639a1e51bdc9ac7917d102b2c1d755c09f8d3
Piotr Lewandowski <p.lewandowski2@student.mini.pw.edu.pl>
2021-02-11 19:14:11 +0100

    Refactors

--- Rendering/PolygonFill/Vertex.cs
+++ Rendering/PolygonFill/Vertex.cs
@@ -4,17 +4,21 @@ namespace Rendering.PolygonFill
 {
     public class Vertex
     {
-        public Vertex(Vector3 p, Vector3 normalVector)
+        public Vertex(Vector3 point, Vector3 normalVector)
         {
             NormalVector = normalVector;
-            fX = p.X;
-            fY = p.Y;
-            fZ = p.Z;
+            fX = point.X;
+            fY = point.Y;
+            fZ = point.Z;
             iX = (int) fX;
             iY = (int) fY;
             iZ = (int) fZ;
+            AsVector3 = new Vector3(point.X, point.Y, point.Z);
+            AsVector2 = new Vector2(point.X, point.Y);
         }
 
+        public Vector3 AsVector3 { get; }
+        public Vector2 AsVector2 { get; }
         public Vector3 NormalVector { get; }
         public Vertex Next { get; set; }
         public Vertex Previous { get; set; }
