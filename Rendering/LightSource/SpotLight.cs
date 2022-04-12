aa88125067349c36b570ad9ae7dc5052e8e0b09e
Piotr Lewandowski <p.lewandowski2@student.mini.pw.edu.pl>
2022-04-12 15:23:09 +0200

    Code cleanup in light sources

--- Rendering/LightSource/SpotLight.cs
+++ Rendering/LightSource/SpotLight.cs
@@ -4,18 +4,10 @@ using System.Windows.Media;
 
 namespace Rendering.LightSource
 {
-    public record SpotLight : LightSource
+    public record SpotLight(Color Color, Vector3 Position, Vector3 Direction, float CutOffAngle) : LightSource(Color,
+        Position)
     {
-        public SpotLight(Color color, Vector3 position, Vector3 direction, float cutOffAngle) : base(color, position)
-        {
-            Direction = direction;
-            CutOffAngle = cutOffAngle;
-            IntensityVector = new Vector3(color.R, color.G, color.B) / 255;
-        }
-
-        public float CutOffAngle { get; }
-        public Vector3 Direction { get; }
-        public Vector3 IntensityVector { get; }
+        public Vector3 IntensityVector { get; } = new Vector3(Color.R, Color.G, Color.B) / 255;
         public float Power { get; } = 10;
         public override Vector3 LightVector(Vector3 point) => Position - point;
 
